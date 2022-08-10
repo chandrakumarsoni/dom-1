728065c241a1ef0efd0bfe091c440bd38632c249
imchandusoni <soni@fastrack-solutions.com>
2022-08-10 16:53:29 +0530

    index and dom.js created

---
+++ dom.js
@@ -0,0 +1,5 @@
+const header = document.querySelector('h1');
+header.style.margin = '20px';
+header.style.color = 'crimson';
+header.style.fontSize= '50px';
+header.textContent = "This is my first dom"
